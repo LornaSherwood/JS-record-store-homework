42af257e6a704c9db3540e7e7db3b0cad71283df
Lorna Sherwood <lornasherwood@gmail.com>
2017-02-11 19:49:18 +0000

    add change to inventory to show quantity - not passing completely

--- recordStore.js
+++ recordStore.js
@@ -18,9 +18,50 @@ RecordStore.prototype = {
   //     number += 1;
   //   }
   // }
-  listInventory: function(){
-    return this.inventory;
+  // listInventory: function(){
+  //   return this.inventory;
+  // },
+  checkInventory: function(record, list){
+    if (list === []){
+      return true; // not necessary?
+    } else {
+      for (var entry of list){  //
+        var result = true;
+        if (entry['title'] === record.title){
+          result = false;
+        // } else {
+        //   return true;
+        }
+      }
+      return result;
+    }
   },
+
+  listInventory: function(){
+    var inventoryDisplay = [];
+
+    for (var record of this.inventory){
+      if ((inventoryDisplay.length === 0) || this.checkInventory(record, inventoryDisplay)){
+        var entry = {
+          artist: record.artist,
+          title: record.title,
+          price: record.price,
+          quantity: 1
+        }
+        inventoryDisplay.push(entry);
+      }
+       else {
+        for (item of inventoryDisplay){
+          if (item['title'] === record.title){
+          item['quantity'] += 1;
+          }
+        }
+      }
+    }
+
+    return inventoryDisplay;
+  },
+
 
   sellRecord: function(record){
     var index = this.inventory.indexOf(record);
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -40,29 +40,43 @@ describe('recordStore', function() {
     assert.equal(2, recordStore.inventory.length);
   }),
 
+  it('should be able to check inventory if not there', function(){
+    recordStore.addRecord(record1);
+    var list = [{title: "Don't waste your wishes"}, {title: "random title"}];
+    assert.equal(true, recordStore.checkInventory(record1, list));
+  }),
+
+  it('should be able to check inventory if there', function(){
+    recordStore.addRecord(record1);
+    var list = [{title: "Don't waste your wishes"}, {title: "Don't waste your wishes"}];
+    assert.equal(false, recordStore.checkInventory(record1, list));
+  }),
+
   it('should be able to list inventory', function(){
     recordStore.addRecord(record1);
+    recordStore.addRecord(record1);
     recordStore.addRecord(record2);
-    assert.equal(recordStore.inventory, recordStore.listInventory());
-  })
+    assert.equal({ artist: "Killers", title: "Don't waste your wishes", price: 12, quantity: 2 }, recordStore.listInventory()[0]);
+    assert.equal({ artist: "Franz Ferdinand", title: "Tonight", price: 11, quantity: 1}, recordStore.listInventory()[1]);
+  }),
 
   it ('should be able to sell a record and record be removed', function(){
     recordStore.addRecord(record1);
     recordStore.sellRecord(record1);
     assert.equal(0, recordStore.inventory.length);
-  })
+  }),
 
   it ('should be able to sell a record and the balance go up', function(){
     recordStore.addRecord(record1);
     recordStore.sellRecord(record1);
     assert.equal(12, recordStore.balance);
-  })
+  }),
 
   it ('should show total value of inventory', function(){
     recordStore.addRecord(record1);
     recordStore.addRecord(record2);
     assert.equal(23, recordStore.inventoryValue())
-  })
+  }),
 
   it ('should report financial status', function(){
     recordStore.addRecord(record1);
@@ -71,4 +85,4 @@ describe('recordStore', function() {
     assert.equal("Balance: 12, Inventory Value: 11", recordStore.financialReport())
   })
 
-})
+});
